ba295bcc1062aa55e380261c6a9ca107eec86409
AliceMeneses <alicesilvademeneses@gmail.com>
2021-06-04 11:51:09 -0300

    Delete /topicos/{id} - Deletar um topico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -75,5 +76,12 @@ public class TopicosController {
 		Topico topico = atualizarTopicoDto.atualizar(id, topicoRepository);
 		
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletar(@PathVariable Long id){
+		topicoRepository.deleteById(id);
+		
+		return ResponseEntity.ok().build();
 	}
 }
