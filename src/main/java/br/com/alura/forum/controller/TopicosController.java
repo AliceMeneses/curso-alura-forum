db7b0bc1494fe7c2ed5a79d00f5c6ffbde0e4733
AliceMeneses <alicesilvademeneses@gmail.com>
2021-06-05 18:38:41 -0300

    Adicionar ordenação do topicos por atributo no Get /topicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -41,9 +42,9 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam Integer pagina, @RequestParam Integer quantidade) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam Integer pagina, @RequestParam Integer quantidade, @RequestParam String ordenacao) {
 
-		Pageable paginacao = PageRequest.of(pagina, quantidade);
+		Pageable paginacao = PageRequest.of(pagina, quantidade, Direction.ASC, ordenacao);
 
 		if (nomeCurso == null) {			
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
