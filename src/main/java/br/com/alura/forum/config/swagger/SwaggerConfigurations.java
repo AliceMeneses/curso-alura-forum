ba84cb22287c6802636fe431946b4b873cd9eab9
AliceMeneses <alicesilvademeneses@gmail.com>
2021-06-12 18:40:51 -0300

    Adicionar campo para o header Authorization em todos os endpoints na documentação

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -1,11 +1,15 @@
 package br.com.alura.forum.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.alura.forum.modelo.Usuario;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.service.ParameterType;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,8 +23,15 @@ public class SwaggerConfigurations {
                 .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(Usuario.class);
-                
+                .ignoredParameterTypes(Usuario.class)
+                .globalRequestParameters(
+                        Arrays.asList(new RequestParameterBuilder()
+                                .name("Authorization")
+                                .description("Header para token JWT")
+                                .in(ParameterType.HEADER)
+                                .required(false)
+                                .build()));
+               
     }
 
 }
