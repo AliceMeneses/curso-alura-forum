22e9746fa4917306488a5c8fc3efa8f492511309
AliceMeneses <alicesilvademeneses@gmail.com>
2021-06-18 20:35:49 -0300

    Trocar classes do JUnit 4 por JUnit Jupiter

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -1,6 +1,6 @@
 package br.com.alura.forum.repository;
 
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -28,8 +28,8 @@ public class CursoRepositoryTest {
 		
 		Curso curso = cursoRepository.findByNome(nomeCurso);
 		
-		Assert.assertNotNull(curso);
-		Assert.assertEquals(nomeCurso, curso.getNome());
+		Assertions.assertNotNull(curso);
+		Assertions.assertEquals(nomeCurso, curso.getNome());
 	}
 	
 	@Test
@@ -38,7 +38,7 @@ public class CursoRepositoryTest {
 		
 		Curso curso = cursoRepository.findByNome(nomeCurso);
 		
-		Assert.assertNull(curso);
+		Assertions.assertNull(curso);
 	}
 
 }
